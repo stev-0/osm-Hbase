56b6ee0d137ec42e81f01f10772ecc3c07f84567
Stephen Knox <stephenknox73@gmail.com>
2015-09-14 22:45:58 +0000

    correct another error

--- scripts/jython/import_osc.py
+++ scripts/jython/import_osc.py
@@ -76,7 +76,7 @@ class CountingHandler(ContentHandler):
 		row = Put(self.wayId)
 		row.add('wayData','user',self.wayUser)
 		if len(self.wayNodes) > 0:
-		   row.add('wayData','nodes',"#".join("%s" for tag in self.nodeTags))
+		   row.add('wayData','nodes',"#".join("%s" for tag in self.wayNodes))
 		   self.wayNodes = []
 		if len(self.wayTags) > 0:
 		   row.add('wayData','wayTags', "#".join("(%s,%s)" % tup for tup in self.wayTags))
